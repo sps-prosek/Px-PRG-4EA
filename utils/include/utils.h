a0fd46197bd8a54b44f736d23b2fea79b3ed2f6e
Josef Vágner <48518988+josefvagner@users.noreply.github.com>
2024-09-30 15:33:14 +0200

    add sample programs

---
+++ utils/include/utils.h
@@ -0,0 +1,34 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+/* pico SDK libraries */
+#include "pico/time.h"
+
+// Macro to convert milliseconds to microseconds
+#define MS_TO_US(x) (uint32_t)((x) * 1e3)
+
+// Macro to convert microseconds to milliseconds
+#define US_TO_MS(x) (float_t)((x) / 1e3)
+
+// Macro to convert milliseconds to seconds
+#define MS_TO_S(x) (float_t)((x) / 1e3)
+
+// Macro to convert seconds to milliseconds
+#define S_TO_MS(x) (uint32_t)((x) * 1e3)
+
+// Macro to convert microseconds to seconds
+#define US_TO_S(x) (float_t)((x) / 1e6)
+
+// Macro to convert seconds to microseconds
+#define S_TO_US(x) (uint32_t)((x) * 1e6)
+
+// Macro to crop a value between a minimum and maximum
+#define CROP(x, min, max) (x < min ? min : (x > max ? max : x))
+
+// Function to get the current time in milliseconds
+uint32_t millis();
+
+// Function to get the current time in microseconds
+uint32_t micros();
+
+#endif // UTILS_H
---
+++ utils/src/utils.c
@@ -0,0 +1,24 @@
+/**
+ * @file utils.c
+ * @brief Utility functions for the Pico project.
+ */
+
+#include "utils.h"
+
+/**
+ * @brief Get the current time in milliseconds.
+ * @return The current time in milliseconds.
+ */
+uint32_t millis()
+{
+    return to_ms_since_boot(get_absolute_time());
+}
+
+/**
+ * @brief Get the current time in microseconds.
+ * @return The current time in microseconds.
+ */
+uint32_t micros()
+{
+    return to_us_since_boot(get_absolute_time());
+}

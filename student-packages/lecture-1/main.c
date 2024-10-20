43f124718f586c04c641817e911e01b1260b070d
Josef Vágner <48518988+josefvagner@users.noreply.github.com>
2024-10-20 11:24:46 +0200

    reorder and add lecture 1_2

---
+++ student-packages/lecture-1/main.c
@@ -0,0 +1,25 @@
+// Pico SDK libraries
+#include "pico/stdlib.h"
+
+#define LED_PIN 25
+#define BLINK_DELAY_MS 500
+
+int main()
+{
+    // Initialize GPIO pins
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // Infinite loop
+    while (1)
+    {
+        // Turn on the LED
+        gpio_put(LED_PIN, 1);
+        sleep_ms(BLINK_DELAY_MS);
+
+        // Turn off the LED
+        gpio_put(LED_PIN, 0);
+        sleep_ms(BLINK_DELAY_MS);
+    }
+    return 0;
+}
